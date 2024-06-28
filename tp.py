2389b22ddacbdc8ecd7d49652bc5b07782955393
valentinabz <valentina.belen.bv.2007@gmail.com>
2024-06-28 08:28:38 -0300

    Update tp.py

--- tp.py
+++ tp.py
@@ -1,9 +1,13 @@
+#mostrar menu
+#funcines definidas
 def mostrar_menu(sabores):
     print("Bienvenido a la tienda de galletas!")
     print("Estos son los sabores disponibles:")
     for i, sabor in enumerate(sabores):
         print(f"{i + 1}. {sabor}")
 
+#condicionales
+#elegir sabores
 def elegir_sabores(sabores):
     eleccion = []
     while len(eleccion) < 6:
@@ -17,11 +21,14 @@ def elegir_sabores(sabores):
             print("Por favor, ingresa un número válido.")
     return eleccion
 
+#bucles
+#mostrar los sabores
+
 def mostrar_seleccion(seleccion):
     print("Has elegido los siguientes sabores:")
     for sabor in seleccion:
         print(f"- {sabor}")
-
+#modiicar o quitar algun sabor
 def modificar_seleccion(seleccion, sabores):
     while True:
         opcion = input("¿Quieres quitar algún sabor? (s/n): ").lower()
@@ -38,7 +45,7 @@ def modificar_seleccion(seleccion, sabores):
         else:
             print("Opción no válida. Intenta de nuevo.")
     return seleccion
-
+#funcion para elegir los sabores
 def elegir_nuevo_sabor(sabores, seleccion):
     while True:
         try:
@@ -49,13 +56,15 @@ def elegir_nuevo_sabor(sabores, seleccion):
                 print("Opción inválida o sabor ya elegido. Intenta de nuevo.")
         except :
             print("Por favor, ingresa un número válido.")
-
+#finalizar la compra
 def finalizar_compra(seleccion):
     print("Gracias por tu compra! Has elegido los siguientes sabores de galletas:")
     for sabor in seleccion:
         print(f"- {sabor}")
     print("Gracias,¡Que disfrutes tus galletas!")
+#lista
 
+#sabores
 def main():
     sabores = [
         "Chocolate", "Chocoalmendra", "Red velvet", "Limon", "Coco", "m&m", 
